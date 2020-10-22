0b1b77da536fdd05634af6c5fb3f1df76caccf96
Naufaldy Rooswandy <naufaldymdrp@outlook.com>
2020-10-22 15:05:37 +0700

    #4, MRKM2-23 #comment penyelesaian sub-issue MRKM2-26 #time 8h 0m MRKM2-26 #comment sudah dilakukan penambahan _DialogForm.cshtml sebagai tempat form untuk fungsi tambah (MRKM2-27), detail (MRKM2-28), edit (MRKM2-29), dan hapus (MRKM2-30). Namun sub-issue ini baru dianggap selesai apabila ke-4 fitur tersebut sudah diselesaikan #time 8h 0m

--- Markom2.Repository/Models/MUser.cs
+++ Markom2.Repository/Models/MUser.cs
@@ -14,21 +14,32 @@ namespace Markom2.Repository.Models
         public int Id { get; set; }
         
         [Required]
+        [MaxLength(50)]
         [Column(TypeName = "varchar(50)")]        
         public string Username { get; set; }
 
         [Required]
+        [MaxLength(50)]
         [Column(TypeName = "varchar(50)")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [NotMapped]
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Retype Password")]
+        [DataType(DataType.Password)]
+        public string RetypePassword { get; set; }
+
         [Column("M_Role_Id")]
+        [Display(Name = "Role")]
         public int MRoleId { get; set; }
 
         [ForeignKey("MRoleId")]
         public MRole MRole_Navigation { get; set; }
 
         [Column("M_Employee_Id")]
+        [Display(Name = "Employee")]
         public int MEmployeeId { get; set; }
 
         [ForeignKey("MEmployeeId")]
